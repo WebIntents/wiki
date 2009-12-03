5bdb02149f8ce23279eb56c7cd7695d7f5e1bce7
justin.forest <devnull@localhost>
2009-12-03 15:31:24 +0300

    Added <base> to use namespaces correctly.

--- base.py
+++ base.py
@@ -108,6 +108,8 @@ class BaseRequestHandler(webapp.RequestHandler):
     # We'll display the user name if available and the URL on all pages
     values = {'user': user, 'log_in_out_url': log_in_out_url, 'editing': self.request.get('edit'), 'is_admin': users.is_current_user_admin() }
     values['sidebar'] = pages.cache.get('sidebar', create=True)
+    url = urlparse.urlparse(self.request.url)
+    values['base'] = url[0] + '://' + url[1]
     values.update(template_values)
 
     logging.debug(values)
