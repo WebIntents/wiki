096eccb17262233bf9ca479c9dc73e79d5c75c86
justin.forest <devnull@localhost>
2011-04-09 00:51:34 +0400

    Indentation should be 4 spaces long.

--- model.py
+++ model.py
@@ -6,7 +6,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.gnu.org/licenses/gpl.html
+#         http://www.gnu.org/licenses/gpl.html
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -20,56 +20,56 @@ __author__ = 'justin.forest@gmail.com'
 from google.appengine.ext import db
 
 class WikiUser(db.Model):
-  wiki_user = db.UserProperty()
-  joined = db.DateTimeProperty(auto_now_add=True)
-  wiki_user_picture = db.BlobProperty()
-  user_feed = db.StringProperty()
+    wiki_user = db.UserProperty()
+    joined = db.DateTimeProperty(auto_now_add=True)
+    wiki_user_picture = db.BlobProperty()
+    user_feed = db.StringProperty()
 
 
 class WikiContent(db.Model):
-  """
-  Stores current versions of pages.
-  """
-  title = db.StringProperty(required=True)
-  body = db.TextProperty(required=False)
-  author = db.ReferenceProperty(WikiUser)
-  updated = db.DateTimeProperty(auto_now_add=True)
-  pread = db.BooleanProperty()
-  # The name of the page that this one redirects to.
-  redirect = db.StringProperty()
-  # Labels used by this page.
-  labels = db.StringListProperty()
-  # Pages that this one links to.
-  links = db.StringListProperty()
+    """
+    Stores current versions of pages.
+    """
+    title = db.StringProperty(required=True)
+    body = db.TextProperty(required=False)
+    author = db.ReferenceProperty(WikiUser)
+    updated = db.DateTimeProperty(auto_now_add=True)
+    pread = db.BooleanProperty()
+    # The name of the page that this one redirects to.
+    redirect = db.StringProperty()
+    # Labels used by this page.
+    labels = db.StringListProperty()
+    # Pages that this one links to.
+    links = db.StringListProperty()
 
-  @classmethod
-  def get_by_key(cls, key):
-    return db.get(db.Key(key))
+    @classmethod
+    def get_by_key(cls, key):
+        return db.get(db.Key(key))
 
-  def put(self):
-    "Adds the gaewiki:parent: labels transparently."
-    labels = [l for l in self.labels if not l.startswith('gaewiki:parent:')]
-    if '/' in self.title:
-      parts = self.title.split('/')[:-1]
-      while parts:
-        label = 'gaewiki:parent:' + '/'.join(parts)
-        labels.append(label)
-        parts.pop()
-        break # remove to add recursion
-    self.labels = labels
-    db.Model.put(self)
+    def put(self):
+        "Adds the gaewiki:parent: labels transparently."
+        labels = [l for l in self.labels if not l.startswith('gaewiki:parent:')]
+        if '/' in self.title:
+            parts = self.title.split('/')[:-1]
+            while parts:
+                label = 'gaewiki:parent:' + '/'.join(parts)
+                labels.append(label)
+                parts.pop()
+                break # remove to add recursion
+        self.labels = labels
+        db.Model.put(self)
 
 
 class WikiRevision(db.Model):
-  """
-  Stores older revisions of pages.
-  """
-  title = db.StringProperty()
-  wiki_page = db.ReferenceProperty(WikiContent)
-  revision_body = db.TextProperty(required=True)
-  author = db.ReferenceProperty(WikiUser)
-  created = db.DateTimeProperty(auto_now_add=True)
-  pread = db.BooleanProperty()
+    """
+    Stores older revisions of pages.
+    """
+    title = db.StringProperty()
+    wiki_page = db.ReferenceProperty(WikiContent)
+    revision_body = db.TextProperty(required=True)
+    author = db.ReferenceProperty(WikiUser)
+    created = db.DateTimeProperty(auto_now_add=True)
+    pread = db.BooleanProperty()
 
 
 def get_by_key(key):
