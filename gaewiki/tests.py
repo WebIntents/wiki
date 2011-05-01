ad49229fd175622d95372a6ac1d3a9dee4e7dd22
justin.forest <devnull@localhost>
2011-05-01 15:19:02 +0400

    Tests now use the testbed.

--- gaewiki/tests.py
+++ gaewiki/tests.py
@@ -2,15 +2,52 @@
 
 import unittest
 
-import access
+from google.appengine.ext import testbed
 
-class WhiteListedPage(unittest.TestCase):
+import access
+import settings
+import util
+
+
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        self.testbed = testbed.Testbed()
+        self.testbed.activate()
+        self.testbed.init_datastore_v3_stub()
+
+    def tearDown(self):
+        self.testbed.deactivate()
+
+
+class PackPageHeader(TestCase):
+    """Tests whether page haders can be built correctly."""
     def runTest(self):
-        self.assertEquals(True, access.is_page_whitelisted('Welcome'))
+        header = util.pack_page_header({
+            'simple': 'foo',
+            'list': ['foo', 'bar'],
+            'text': 'must be ignored',
+        })
+        self.assertEquals('list: foo, bar\nsimple: foo', header)
+
+
+class SettingsChange(TestCase):
+    """Makes sure that the settings can be changed, since many other tests rely
+    on that."""
+    def runTest(self):
+        self.assertEquals(None, settings.get('no-such-value'))
+        settings.change({ 'no-such-value': 'yes' })
+        self.assertEquals('yes', settings.get('no-such-value'))
+
+
+class WhiteListedPage(TestCase):
+    def runTest(self):
+        self.assertEquals(False, access.is_page_whitelisted('Welcome'))
 
 
 def run_tests():
     suite = unittest.TestSuite()
+    suite.addTest(PackPageHeader())
+    suite.addTest(SettingsChange())
     suite.addTest(WhiteListedPage())
     unittest.TextTestRunner().run(suite)
 
