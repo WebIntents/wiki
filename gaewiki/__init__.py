f738f2b06abe300efff49a3a77fea6b8789c82a6
justin.forest <devnull@localhost>
2011-11-30 18:57:43 +0300

    Use the main() function for better performance.
    Update issue 55:
    Use the main() function.

--- gaewiki/__init__.py
+++ gaewiki/__init__.py
@@ -10,10 +10,19 @@ use_library('django', '1.2')
 
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp.util import run_wsgi_app
 
 import handlers
 
-if __name__ == '__main__':
+
+application = webapp.WSGIApplication(handlers.handlers)
+
+
+def main():
+    run_wsgi_app(application)
+
+
+if __name__ == "__main__":
     debug = os.environ['SERVER_SOFTWARE'].startswith('Development/')
     if debug:
         logging.getLogger().setLevel(logging.DEBUG)
@@ -21,4 +30,4 @@ if __name__ == '__main__':
     sys.path.insert(0, os.path.dirname(__file__))
     template.register_template_library('templatetags.filters')
 
-    wsgiref.handlers.CGIHandler().run(webapp.WSGIApplication(handlers.handlers, debug=debug))
+    main()
